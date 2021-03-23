d8ab5ada5d1c493b0a57d615088c935db9320a86
Fernando Guillen <fguillen.mail@gmail.com>
2021-03-23 12:53:10 +0100

    Head also moves and rotates towards Target

---
+++ Assets/Scripts/FlipTowardsTargetController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipTowardsTargetController : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+
+    void Update()
+    {
+        if(
+            (_target.position.x < transform.position.x && transform.localScale.y > 0) ||
+            (_target.position.x > transform.position.x && transform.localScale.y < 0)
+        )
+        {
+            transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
---
+++ Assets/Scripts/MoveTowardsTargetController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsTargetController : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    [SerializeField] Transform _target;
+
+    void Update()
+    {
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+        transform.position = newPosition;
+    }
+}
