ab3ca923a665a6b687e920af43479f7e8da82fb3
Fernando Guillen <fguillen.mail@gmail.com>
2021-03-23 19:18:08 +0100

    Target follows the cursor automatically

---
+++ Assets/Scripts/MoveTowardsCursorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsCursorController : MonoBehaviour
+{
+    [SerializeField] float _speed;
+
+    void Update()
+    {
+        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector3.MoveTowards(transform.position, cursorPosition, _speed * Time.deltaTime);
+    }
+}
