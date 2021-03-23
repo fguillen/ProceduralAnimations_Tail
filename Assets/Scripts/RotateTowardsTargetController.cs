d2d7c3bc3bf11ff968c6b7108711057f40c283fd
Fernando Guillen <fguillen.mail@gmail.com>
2021-03-23 12:39:53 +0100

    Head is rotation toward Target

---
+++ Assets/Scripts/RotateTowardsTargetController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RotateTowardsTargetController : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    [SerializeField] float _speed;
+
+    void Update()
+    {
+        Vector2 direction = _target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _speed * Time.deltaTime);
+    }
+}
