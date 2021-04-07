9f841569e8d009d93bf31886f306b1306bfa9f38
Fernando Guillen <fguillen.mail@gmail.com>
2021-04-07 21:20:38 +0200

    Bird is walking better

--- Assets/Scripts/BirdWalkerController.cs
+++ Assets/Scripts/BirdWalkerController.cs
@@ -12,25 +12,55 @@ public class BirdWalkerController : MonoBehaviour
     [SerializeField] Transform ikTargetRear;
     [SerializeField] Transform ikTargetFront;
 
+    [SerializeField] float feetSpeed = 1.0f;
+
     [SerializeField] float maxTargetDistance;
+
+    bool _movingRear = false;
+    bool _movingFront = false;
 
 
     // Update is called once per frame
     void Update()
     {
-        CheckIfRearTargetShouldMove();
-        CheckIfFrontTargetShouldMove();
+        if(!_movingRear)
+            CheckIfRearTargetShouldMove();
+
+        if(!_movingFront)
+            CheckIfFrontTargetShouldMove();
+
+        if(_movingRear)
+            MoveRear();
+
+        if(_movingFront)
+            MoveFront();
+    }
+
+    void MoveRear()
+    {
+        ikTargetRear.position = Vector3.MoveTowards(ikTargetRear.position, footPointRear.position, feetSpeed);
+
+        if(ikTargetRear.position == footPointRear.position)
+            _movingRear = false;
+    }
+
+    void MoveFront()
+    {
+        ikTargetFront.position = Vector3.MoveTowards(ikTargetFront.position, footPointFront.position, feetSpeed);
+
+        if(ikTargetFront.position == footPointFront.position)
+            _movingFront = false;
     }
 
     void CheckIfRearTargetShouldMove()
     {
         if(Vector3.Distance(ikEffectorRear.position, footPointRear.position) > maxTargetDistance)
-            ikTargetRear.position = footPointRear.position;
+            _movingRear = true;
     }
 
     void CheckIfFrontTargetShouldMove()
     {
         if(Vector3.Distance(ikEffectorFront.position, footPointFront.position) > maxTargetDistance)
-            ikTargetFront.position = footPointFront.position;
+            _movingFront = true;
     }
 }
