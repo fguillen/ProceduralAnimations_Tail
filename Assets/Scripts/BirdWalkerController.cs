815b46a6d7767ed7d1f9bf9a46133eba46e09660
Fernando Guillen <fguillen.mail@gmail.com>
2021-04-07 21:08:44 +0200

    Bird start walking

---
+++ Assets/Scripts/BirdWalkerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdWalkerController : MonoBehaviour
+{
+    [SerializeField] Transform bird;
+    [SerializeField] Transform footPointRear;
+    [SerializeField] Transform footPointFront;
+    [SerializeField] Transform ikEffectorRear;
+    [SerializeField] Transform ikEffectorFront;
+    [SerializeField] Transform ikTargetRear;
+    [SerializeField] Transform ikTargetFront;
+
+    [SerializeField] float maxTargetDistance;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfRearTargetShouldMove();
+        CheckIfFrontTargetShouldMove();
+    }
+
+    void CheckIfRearTargetShouldMove()
+    {
+        if(Vector3.Distance(ikEffectorRear.position, footPointRear.position) > maxTargetDistance)
+            ikTargetRear.position = footPointRear.position;
+    }
+
+    void CheckIfFrontTargetShouldMove()
+    {
+        if(Vector3.Distance(ikEffectorFront.position, footPointFront.position) > maxTargetDistance)
+            ikTargetFront.position = footPointFront.position;
+    }
+}
